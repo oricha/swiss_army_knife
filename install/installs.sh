514d231aba88dbbae6b9ef515178f9b99fb138ee
Karel Muñiz Perdomo <oricha@users.noreply.github.com>
2024-12-31 01:39:39 +0100

    Update installs.sh
    Added new tools

--- install/installs.sh
+++ install/installs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "About to install logs of tools and settings developers need..."
+echo "About to install tools and settings developers need..."
 echo "Are you sure? [y/N] "
 read -s response
 response=$(echo "$response" | tr '[:upper:]' '[:lower:]') # to lower
@@ -11,29 +11,37 @@ else
     echo "Skipping..."
     exit
 fi
+
+echo "Updating Homebrew..."
+brew update
+
 echo "Installing mandatory tools..."
-brew tap caskroom/versions
-brew cask install java
 brew install sbt
 brew install scala
 brew install gradle
-brew install awscli
 brew install coreutils
 brew install python
 brew install pip
 brew install git
 
+# Java installation with OpenJDK
+brew install --cask temurin
+
+# AWS CLI installation
+brew install awscli
 
 echo "Other Cask applications..."
-brew cask install iterm2
-brew cask install caffeine
-brew cask install docker-edge
-brew cask install go2shell
-brew cask install postman
-brew cask install xquartz
-brew cask install alfred
-brew cask install textmate
-brew cask install fluor
+brew install --cask iterm2
+brew install --cask caffeine
+brew install --cask docker
+brew install --cask go2shell
+brew install --cask postman
+brew install --cask xquartz
+brew install --cask alfred
+brew install --cask textmate
+brew install --cask fluor
+brew install --cask visual-studio-code
+brew install --cask slack
 
 echo "Other CLI applications..."
 brew install maven
@@ -55,11 +63,14 @@ brew install tree
 brew install node
 brew install yarn --without-node
 
-echo "Fun cli commands..."
+echo "Fun CLI commands..."
 brew install cmatrix
 brew install cowsay
 brew install ponysay
 brew install figlet
 
 echo "Pip installs..."
+pip3 install --upgrade pip
 pip3 install requests
+
+echo "All done!"
